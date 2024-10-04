496fd2cecaf8ec1a8feccd6789056cc809b5ce75
Avi-020 <avipawar020@gmail.com>
2024-10-04 11:33:25 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Gallery from './Components/Gallery';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [error, setError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username === 'Akshu' && password === '1322') {
+      setIsLoggedIn(true); // Set login status to true if credentials are correct
+      setError(''); // Clear any previous errors
+    } else {
+      setError('Wrong Password'); // Set error if login fails
+    }
+  };
+
+  if (isLoggedIn) {
+    return (
+        <Gallery/>
+     
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+    <div className='container mt-5'  >
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">User Name</label>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Name" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input type="password"  value={password}  onChange={(e) => setPassword(e.target.value)} className="form-control" id="exampleInputPassword1" placeholder="Password" />
+        </div>
+        <div className="form-group form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" className="btn btn-primary">Login</button>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+
+      </form>
     </div>
+    </>
+    
   );
 }
 
---
+++ src/Components/Gallery.js
@@ -0,0 +1,172 @@
+import React from 'react';
+import {
+  MDBCol,
+  MDBRow,
+} from 'mdb-react-ui-kit';
+
+import img1 from "../images/img (1).jpeg"
+import img2 from "../images/img (2).jpeg"
+import img3 from "../images/img (3).jpeg"
+import img4 from "../images/img (4).jpeg"
+import img5 from "../images/img (5).jpeg"
+import img6 from "../images/img (6).jpeg"
+import img7 from "../images/img (7).jpeg"
+import img8 from "../images/img (8).jpeg"
+import img9 from "../images/img (9).jpeg"
+import img10 from "../images/img (10).jpeg"
+import img11 from "../images/img (11).jpeg"
+import img12 from "../images/img (12).jpeg"
+import img13 from "../images/img (13).jpeg"
+import img14 from "../images/img (14).jpeg"
+import img15 from "../images/img (15).jpeg"
+import img16 from "../images/img (16).jpeg"
+import img17 from "../images/img (17).jpeg"
+import img18 from "../images/img (18).jpeg"
+import img19 from "../images/img (19).jpeg"
+import img20 from "../images/img (20).jpeg"
+
+
+function Gallery() {
+  return (
+    <div>
+       <MDBRow>
+      <MDBCol lg={4} md={12} className='mb-4 mb-lg-0'>
+        <img
+          src={img1}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Boat on Calm Water'
+        />
+
+        <img
+          src={img2}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Wintry Mountain Landscape'
+        />
+      </MDBCol>
+
+      <MDBCol lg={4} className='mb-4 mb-lg-0'>
+        <img
+          src={img3}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Mountains in the Clouds'
+        />
+
+        <img
+          src={img4}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Boat on Calm Water'
+        />
+      </MDBCol>
+
+      <MDBCol lg={4} className='mb-4 mb-lg-0'>
+        <img
+          src={img5}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Waves at Sea'
+        />
+
+        <img
+          src={img6}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Yosemite National Park'
+        />
+      </MDBCol>
+
+      <MDBCol lg={4} className='mb-4 mb-lg-0'>
+        <img
+          src={img7}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Waves at Sea'
+        />
+
+        <img
+          src={img8}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Yosemite National Park'
+        />
+      </MDBCol>
+      <MDBCol lg={4} className='mb-4 mb-lg-0'>
+        <img
+          src={img9}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Waves at Sea'
+        />
+
+        <img
+          src={img10}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Yosemite National Park'
+        />
+      </MDBCol>
+      <MDBCol lg={4} className='mb-4 mb-lg-0'>
+        <img
+          src={img11}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Waves at Sea'
+        />
+
+        <img
+          src={img12}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Yosemite National Park'
+        />
+      </MDBCol>
+      <MDBCol lg={4} className='mb-4 mb-lg-0'>
+        <img
+          src={img13}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Waves at Sea'
+        />
+
+        <img
+          src={img14}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Yosemite National Park'
+        />
+      </MDBCol>
+      <MDBCol lg={4} className='mb-4 mb-lg-0'>
+        <img
+          src={img15}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Waves at Sea'
+        />
+
+        <img
+          src={img16}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Yosemite National Park'
+        />
+      </MDBCol>
+      <MDBCol lg={4} className='mb-4 mb-lg-0'>
+        <img
+          src={img17}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Waves at Sea'
+        />
+
+        <img
+          src={img18}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Yosemite National Park'
+        />
+      </MDBCol>
+      <MDBCol lg={4} className='mb-4 mb-lg-0'>
+        <img
+          src={img19}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Waves at Sea'
+        />
+
+        <img
+          src={img20}
+          className='w-100 shadow-1-strong rounded mb-4'
+          alt='Yosemite National Park'
+        />
+      </MDBCol>
+    </MDBRow>
+      
+    </div>
+  )
+}
+
+export default Gallery
