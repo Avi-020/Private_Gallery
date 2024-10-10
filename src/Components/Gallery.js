3fb5614c417eccb7339dd7fcd94f2df6d6aeb393
Avi-020 <avipawar020@gmail.com>
2024-10-10 22:30:48 +0530

    some changes and new images

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -24,150 +24,197 @@ import img17 from "../images/img (17).jpeg"
 import img18 from "../images/img (18).jpeg"
 import img19 from "../images/img (19).jpeg"
 import img20 from "../images/img (20).jpeg"
-import Videos from './Videos';
+import img21 from "../images/img (21).jpeg"
+import img22 from "../images/img (22).jpeg"
+import img23 from "../images/img (23).jpeg"
+import img24 from "../images/img (24).jpeg"
+import img25 from "../images/img (25).jpeg"
+
+
 
 
 function Gallery() {
   return (
     <div>
-      <Videos/> 
-      <h1>hii</h1>
-       <MDBRow>
-      <MDBCol lg={4} md={12} className='mb-4 mb-lg-0'>
-        <img
-          src={img1}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Boat on Calm Water'
-        />
+      <div className='text-center'>
+        <h3 className='blockquote mt-3'>Thanks For Making My Life This Much Beautiful  </h3>
+        <h6 className='blockquote-footer'>Every Photo Can Tell You The Story Of Our Unforgotable Love...</h6>
+        <a href="https://photos.app.goo.gl/g8rLdC2zest2QEqMA">
+          <button type="button" className="btn btn-primary mb-4 ">Watch Vlogs Bbu </button>
+        </a>
+      </div>
 
-        <img
-          src={img2}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Wintry Mountain Landscape'
-        />
-      </MDBCol>
+      <MDBRow>
+        <MDBCol lg={4} md={12} className='mb-4 mb-lg-0'>
+          <img
+            src={img1}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Boat on Calm Water'
+          />
 
-      <MDBCol lg={4} className='mb-4 mb-lg-0'>
-        <img
-          src={img3}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Mountains in the Clouds'
-        />
+          <img
+            src={img2}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Wintry Mountain Landscape'
+          />
+        </MDBCol>
 
-        <img
-          src={img4}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Boat on Calm Water'
-        />
-      </MDBCol>
+        <MDBCol lg={4} className='mb-4 mb-lg-0'>
+          <img
+            src={img3}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Mountains in the Clouds'
+          />
 
-      <MDBCol lg={4} className='mb-4 mb-lg-0'>
-        <img
-          src={img5}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Waves at Sea'
-        />
+          <img
+            src={img4}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Boat on Calm Water'
+          />
+        </MDBCol>
 
-        <img
-          src={img6}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Yosemite National Park'
-        />
-      </MDBCol>
+        <MDBCol lg={4} className='mb-4 mb-lg-0'>
+          <img
+            src={img5}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Waves at Sea'
+          />
 
-      <MDBCol lg={4} className='mb-4 mb-lg-0'>
-        <img
-          src={img7}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Waves at Sea'
-        />
+          <img
+            src={img6}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Yosemite National Park'
+          />
+        </MDBCol>
 
-        <img
-          src={img8}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Yosemite National Park'
-        />
-      </MDBCol>
-      <MDBCol lg={4} className='mb-4 mb-lg-0'>
-        <img
-          src={img9}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Waves at Sea'
-        />
+        <MDBCol lg={4} className='mb-4 mb-lg-0'>
+          <img
+            src={img7}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Waves at Sea'
+          />
 
-        <img
-          src={img10}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Yosemite National Park'
-        />
-      </MDBCol>
-      <MDBCol lg={4} className='mb-4 mb-lg-0'>
-        <img
-          src={img11}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Waves at Sea'
-        />
+          <img
+            src={img8}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Yosemite National Park'
+          />
+        </MDBCol>
+        <MDBCol lg={4} className='mb-4 mb-lg-0'>
+          <img
+            src={img9}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Waves at Sea'
+          />
 
-        <img
-          src={img12}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Yosemite National Park'
-        />
-      </MDBCol>
-      <MDBCol lg={4} className='mb-4 mb-lg-0'>
-        <img
-          src={img13}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Waves at Sea'
-        />
+          <img
+            src={img10}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Yosemite National Park'
+          />
+        </MDBCol>
+        <MDBCol lg={4} className='mb-4 mb-lg-0'>
+          <img
+            src={img11}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Waves at Sea'
+          />
 
-        <img
-          src={img14}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Yosemite National Park'
-        />
-      </MDBCol>
-      <MDBCol lg={4} className='mb-4 mb-lg-0'>
-        <img
-          src={img15}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Waves at Sea'
-        />
+          <img
+            src={img12}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Yosemite National Park'
+          />
+        </MDBCol>
+        <MDBCol lg={4} className='mb-4 mb-lg-0'>
+          <img
+            src={img13}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Waves at Sea'
+          />
 
-        <img
-          src={img16}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Yosemite National Park'
-        />
-      </MDBCol>
-      <MDBCol lg={4} className='mb-4 mb-lg-0'>
-        <img
-          src={img17}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Waves at Sea'
-        />
+          <img
+            src={img14}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Yosemite National Park'
+          />
+        </MDBCol>
+        <MDBCol lg={4} className='mb-4 mb-lg-0'>
+          <img
+            src={img15}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Waves at Sea'
+          />
 
-        <img
-          src={img18}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Yosemite National Park'
-        />
-      </MDBCol>
-      <MDBCol lg={4} className='mb-4 mb-lg-0'>
-        <img
-          src={img19}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Waves at Sea'
-        />
+          <img
+            src={img16}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Yosemite National Park'
+          />
+        </MDBCol>
+        <MDBCol lg={4} className='mb-4 mb-lg-0'>
+          <img
+            src={img17}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Waves at Sea'
+          />
 
-        <img
-          src={img20}
-          className='w-100 shadow-1-strong rounded mb-4'
-          alt='Yosemite National Park'
-        />
-      </MDBCol>
-    </MDBRow>
-      
+          <img
+            src={img18}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Yosemite National Park'
+          />
+        </MDBCol>
+        <MDBCol lg={4} className='mb-4 mb-lg-0'>
+          <img
+            src={img19}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Waves at Sea'
+          />
+
+          <img
+            src={img20}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Yosemite National Park'
+          />
+        </MDBCol>
+        <MDBCol lg={4} className='mb-4 mb-lg-0'>
+          <img
+            src={img21}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Waves at Sea'
+          />
+
+          <img
+            src={img22}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Yosemite National Park'
+          />
+        </MDBCol>
+        <MDBCol lg={4} className='mb-4 mb-lg-0'>
+          <img
+            src={img23}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Waves at Sea'
+          />
+
+          <img
+            src={img24}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Yosemite National Park'
+          />
+        </MDBCol>
+        <MDBCol lg={4} className='mb-4 mb-lg-0'>
+          <img
+            src={img25}
+            className='w-100 shadow-1-strong rounded mb-4'
+            alt='Waves at Sea'
+          />
+
+         
+        </MDBCol>
+      </MDBRow>
+
     </div>
   )
 }
